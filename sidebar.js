23d3df061db374904ac86bd141f498ceae9aad3b
Rabd <rabd.ctee@gmail.com>
2026-05-02 15:59:16 +1000

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -285,7 +285,8 @@
             <li style="background:#f0f6fc;"><a href="#group-staying-safe"    style="padding-left:30px;font-size:0.8em;color:#555;">🔒 Staying Safe</a></li>
             <li style="background:#f0f6fc;"><a href="#group-new-tech"        style="padding-left:30px;font-size:0.8em;color:#555;">🤖 New Technology</a></li>
             ` : ''}
-            <li><a href="contacts.html"${isActive('contacts.html') ? ' class="sb-active"' : ''}>📞 Contacts</a></li>
+            <li><a href="index.html" onclick="if(window.showPage){showPage('contact');return false;}else{window.location.href='index.html?page=contact';return false;}"${isActive('contacts.html') ? ' class="sb-active"' : ''}>📞 Contacts</a></li>
+            <li><a href="index.html" onclick="if(window.showPage){showPage('about');return false;}else{window.location.href='index.html?page=about';return false;">📖 About This Website</a></li>
           </ul>
         </div>
 
